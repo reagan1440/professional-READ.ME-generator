10fb386d5f9e08126db78f856672daa28e3f965f
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2023-12-06 20:46:30 -0500

    added licensing

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    switch (license) {
+        case "none":
+            return "";
+        case "MIT License":
+            return "![License: MIT](https://img.sheilds.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+            case "Apache License 2.0":
+              return  "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+              case "LGPL 3.0":
+                return "[![License: LGPL 3.0](https://img.shields.io/badge/License-LGPL%203.0-blue.svg)](https://opensource.org/licenses/LGPL-3.0)";
+            case "GPL 3.0":
+                return "[![License: GPL 3.0](https://img.shields.io/badge/License-GPL%203.0-blue.svg)](https://opensource.org/licenses/GPL-3.0)";
+            default:
+                return "";
+    }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
